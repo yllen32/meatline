4c370693a0455663853e003fc1fc1a6a84b2b4fc
yllen32 <furtov1995@yandex.ru>
2022-10-25 16:04:52 +0300

    add STATIC_URL in to settings fiel

--- meet_line/meet_line/settings.py
+++ meet_line/meet_line/settings.py
@@ -88,6 +88,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
