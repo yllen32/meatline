edf3edd13a10d7d48bc2833437a89b7017cb9107
yllen32 <furtov1995@yandex.ru>
2022-10-28 02:55:44 +0300

    add postgres settings

--- meet_line/meet_line/settings.py
+++ meet_line/meet_line/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
 
-ALLOWED_HOSTS = ['51.250.15.186', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['51.250.11.15', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -57,10 +57,14 @@ WSGI_APPLICATION = 'meet_line.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
-}
+} 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
